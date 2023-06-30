17f306ac1879dcb029c1840f574fc30db8d2088b
Kaurvi <135301950+Kaurvi@users.noreply.github.com>
2023-06-30 03:05:49 -0400

    Update CtoF.java

--- src/CtoF.java
+++ src/CtoF.java
@@ -1,2 +1,37 @@
-public class CtoF {
+public class CtoF {public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        double tempC = 0.0;
+        double tempF = 0.0;
+        String trash = "";
+        String tryAgain ="Y/N";
+        done = false;
+        do
+        {System.out.print("Enter temp in celsius: ");
+            if (in.hasNextDouble())
+            {
+                tempC = in.nextDouble();
+                tempF = ((tempC*9/5)+32);
+                System.out.println("The temp in farenhiet  " + tempF);
+                if(tempF == 32.0) {
+                    System.out.println("The temperature you've entered is " + tempF + " which is a freezing point!");
+                } else if (tempF == 212.0) {
+                    System.out.println("The temperature you've entered is " + tempF + " which is a boiling point!");
+                }
+            } else
+            {
+                trash = in.next();
+                System.out.println("You must enter a valid temp not: " + trash + "\n");
+            }
+                System.out.println ("wanna try again (Y/N)");
+            in.nextLine ();
+            tryAgain = in.nextLine ();
+            if (tryAgain.equalsIgnoreCase ( "N" ))
+            {
+                done =true;}
+            else if (tryAgain.equalsIgnoreCase ( "Y" ))
+            {done=false;}
+        }
+        while (!done);
+    }
 }
